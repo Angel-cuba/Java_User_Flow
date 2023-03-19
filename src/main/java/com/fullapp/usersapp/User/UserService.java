a677886826dd619863ef9cc224aecc04682fbabf
Mac_Developer <araozangel842@gmail.com>
2023-03-19 22:19:26 +0200

    Add repository and service for user

---
+++ src/main/java/com/fullapp/usersapp/User/UserService.java
@@ -0,0 +1,18 @@
+package com.fullapp.usersapp.User;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService{
+
+  private UserRepository userRepository;
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    return userRepository.findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found"));
+  }
+  
+}
